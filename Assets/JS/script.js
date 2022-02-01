0369d7c858e5c5d6403bda978c07136ef0345ef7
Mattrix01 <emjay.knight@gmail.com>
2022-02-01 14:33:05 +0000

    Targetting save buttons and starting localStorage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
   // creating a function which will be dynamically applied to all the save buttons on click
+  $("button").click(function () {
+    const storedData = initialiseLocalStorage(baseArray);
+    const arr = JSON.parse(localStorage.getItem("schedule"));
+
+    arr[log - 9].text = textBox.value;
+
+    localStorage.setItem("schedule", JSON.stringify(arr));
+
+    console.log("click");
+  });
   // ------- // on click we need to get the value from the textbox 'sibling'
+  var textItem = $("textarea[textInput=TextInputValue]").val();
+  $("textarea").append(textItem);
+
   // ------- // we also need to get the id of the parent
   // ------- // use the id from the parent and the value from the sibling and add to local stroage
   // ------- // notify the user that it was successful for 5 seconds. add and remove element from html
@@ -17,8 +30,8 @@ $(document).ready(function () {
   // -----------------------------------------------
   //this will be done for every 'id'. 9, 10, 11, 12, 13...........Key/Value - local storage
   // ---------------------------------------------------
-  //apply the current time to the 'currentDay' p element in the html using moment.
+  var today = moment();
+  $("#currentDay").text(today.format("dddd, Do MMMM, YYYY kk:mm:ss"));
 });
 
-var today = moment();
-$("#currentDay").text(today.format("dddd, Do MMMM, YYYY kk:mm:ss"));
+// Jquery review vid: https://2u-20.wistia.com/medias/g63k1z1sb3
