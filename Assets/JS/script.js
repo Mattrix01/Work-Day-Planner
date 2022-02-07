3c30ce53b52332bce78319026b57f2b2ecc40d76
Mattrix01 <emjay.knight@gmail.com>
2022-02-07 14:49:32 +0000

    Set colour classes based on time for loop working

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,47 +1,44 @@
-// const initialiseLocalStorage = function (textArea) {
-//   const scheduleData =
-//     JSON.parse(localStorage.getItem("schedule")) || textItem);
+var saveBtn = document.querySelectorAll(".saveBtn");
 
-//   localStorage.setItem("schedule", JSON.stringify(scheduleData));
+// $(document).ready(function () {
+//   // ------- // we also need to get the id of the parent
+//   // ------- // use the id from the parent and the value from the sibling and add to local stroage
+//   // ------- // notify the user that it was successful for 5 seconds. add and remove element from html
+//   // --------------------------------------
+//   //a function which will look at everything time-block and check if past, present or future
+//   // ------ // use moment to find out what the current hour is
+//   // ===== // for each time block we will get its id and compare with current hour
+//   // ----- // - if past, add past class. if present, add present class. if future, add future class.
+//   // ------// the classes PAST, PRESENT, FUTURE already have css applied. just need ot use them.
+//   // -------------------------------------------
+//   // need to call the function above which checks the time blocks
+//   // create an interval which will check the time blocks using the function above, every hour.
+//   // look into local storage - using the id you know exists (for example, 9) - get anything for that id. apply as the value for relevant text area
+//   // -----------------------------------------------
+// });
 
-//   return scheduleData;
-// };
+var today = moment();
+$("#currentDay").text(today.format("dddd, Do MMMM, YYYY kk:mm:ss"));
 
-$(document).ready(function () {
-  // creating a function which will be dynamically applied to all the save buttons on click
-  $("button").click(function () {
-    console.log("click");
-    const storedData = initialiseLocalStorage(textItem);
-    const arr = JSON.parse(localStorage.getItem("schedule"));
+var currentHour = moment().hours();
 
-    arr[log - 9].text = textBox.value;
+var maximumHours = 9;
+var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-    localStorage.setItem("schedule", JSON.stringify(arr));
-  });
-  // ------- // on click we need to get the value from the textbox 'sibling'
-  var textItem = $("textarea[textInput=TextInputValue]").val();
-  $("textarea").append(textItem);
-
-  // ------- // we also need to get the id of the parent
-  // ------- // use the id from the parent and the value from the sibling and add to local stroage
-  // ------- // notify the user that it was successful for 5 seconds. add and remove element from html
-  // --------------------------------------
-  //a function which will look at everything time-block and check if past, present or future
-  // ------ // use moment to find out what the current hour is
-  // ===== // for each time block we will get its id and compare with current hour
-  // ----- // - if past, add past class. if present, add present class. if future, add future class.
-  // ------// the classes PAST, PRESENT, FUTURE already have css applied. just need ot use them.
-  // -------------------------------------------
-  // need to call the function above which checks the time blocks
-  // create an interval which will check the time blocks using the function above, every hour.
-  // look into local storage - using the id you know exists (for example, 9) - get anything for that id. apply as the value for relevant text area
-  // -----------------------------------------------
-  //this will be done for every 'id'. 9, 10, 11, 12, 13...........Key/Value - local storage
-  // ---------------------------------------------------
-  var today = moment();
-  $("#currentDay").text(today.format("dddd, Do MMMM, YYYY kk:mm:ss"));
-});
-
-// Jquery review vid: https://2u-20.wistia.com/medias/g63k1z1sb3
-
-// Targetting save buttons and setting local storage
+for (var i = 1; i <= maximumHours; i++) {
+  //present
+  if (currentHour == workHours[i]) {
+    document.getElementById(i).children[1].classList.add("present");
+    console.log(i);
+  }
+  //past
+  else if (currentHour > workHours[i]) {
+    document.getElementById(i).children[1].classList.add("past");
+    console.log(i);
+  }
+  //future
+  else {
+    document.getElementById(i).children[1].classList.add("future");
+    console.log(i);
+  }
+}
